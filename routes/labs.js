327bfd8b91f6077d6076149d1db9ad8f8904b224
momo <normalguyinvancouver@gmail.com>
2019-10-14 23:20:48 -0700

    save artist data to JSON file

--- routes/labs.js
+++ routes/labs.js
@@ -17,4 +17,31 @@ router.get('/4', (req, res, next) => {
     res.render('lab04')
 });
 
+router.get('/5', (req, res, next) => {
+    res.render('lab05')
+});
+
+router.post('/5', (req, res, next) => {
+    const fs = require('fs')
+
+    let artist = req.body
+    JSON.stringify(artist)
+
+    const path = process.cwd() + '/public/files/artists.json'
+
+    try {
+        fs.readFile(path, function (err, data) {
+            var json = JSON.parse(data);
+            console.log(json);
+            json.push(artist);
+
+            fs.writeFile(path, JSON.stringify(json), function (err) {
+                if (err) throw err;
+            });
+        });
+    } catch (error) {
+        console.error('Error:', error)
+    }
+});
+
 module.exports = router;

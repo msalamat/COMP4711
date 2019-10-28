ce53a402a5ae64b9003d0f8cd1ec4ec33f3019f0
momo <normalguyinvancouver@gmail.com>
2019-10-27 19:29:02 -0700

    dealt with duplicate clicks

--- public/javascripts/memory-game/script.js
+++ public/javascripts/memory-game/script.js
@@ -1,6 +1,6 @@
-let angle = 90
 let element = document.getElementById("game-container");
-let numClicksLeft = 6
+let clicksLeft = 6
+let clickingLock = true
 
 const rotate = () => {
     element.classList.add("rotate-90");
@@ -11,31 +11,65 @@ const rotate = () => {
 const resetColour = () => {
     let bob = [...document.getElementsByClassName("button")].forEach(
         (element, index, array) => {
-            element.style.backgroundColor = "salmon"
+            element.style.backgroundColor = "#f1c40f"
         })
 }
 
 const reset = () => {
     let bob = [...document.getElementsByClassName("button")].forEach( // why do I need to assign a variable? need to store the unpacked?
         (element, index, array) => {
-            element.style.backgroundColor = "salmon"
+            element.style.backgroundColor = "#f1c40f"
         })
 
     element.classList.add("rotate-neg-90");
     element.classList.remove("rotate-90");
-
 }
 
 const clicked = (ref) => {
-    if (numClicksLeft-- > 0) {
-        ref.style.backgroundColor = "palegreen"
-        console.log(numClicksLeft)
+    if (!clickingLock) {
+        let indicies = JSON.parse(localStorage.getItem('indicies'))
+
+        // THE LOGIC
+
+        // do you have clicks remaining?
+        // yes?
+        //      has the button already been clicked?
+        //      (i.e. button has error-img tag
+        //        OR button's color is palegreen)
+        //      yes?
+        //          do nothing
+        //      no?
+        //          clicks--
+        //          is it a successful click?
+        //              give green colour to btn
+        //          is it a failed click?
+        //              give error picture to btn
+        // no?
+        //      do nothing
+
+        if (clicksLeft > 0) {
+            // TODO can use our buddy demorgan to reduce things !(a or b) = !a and !b
+            if (ref.classList.contains('error-img') || (ref.style.backgroundColor) === "palegreen") {
+                console.log('do nothing')
+            } else {
+                --clicksLeft
+                if (indicies.includes(Number(ref.id))) {
+                    ref.style.backgroundColor = "palegreen"
+                } else {
+                    ref.classList.add('error-img')
+                }
+            }
+        } else {
+            console.log('no clicks left')
+        }
     }
 }
 
 const startRound = () => {
+    clickingLock = true
     // generate random unique numbers between 0-24
     let indicies = getIndicies()
+    let indiciesAsArray = Array.from(indicies).sort((a, b) => a - b)
 
     // use indicies to change button colours
     let buttons = [...document.getElementsByClassName("button")]
@@ -43,25 +77,18 @@ const startRound = () => {
 
     filteredButtons.forEach( (element, index, array) => {
         element.style.backgroundColor = "palegreen"
+        element.setAttribute("id", indiciesAsArray[index])
     })
 
+    localStorage.setItem("indicies", JSON.stringify(indiciesAsArray))
+ 
     // hide the color and rotate
     setTimeout(() => {
         rotate()
         resetColour()
+        clickingLock = false
     }, 3000);
 }
-
-
-
-
-// generate random unique numbers between 0-24
-
-// show the button colors matrix briefly
-
-// hide the color 
-
-// rotate
 
 const getIndicies = () => {
     const nums = new Set();
@@ -71,7 +98,6 @@ const getIndicies = () => {
     }
     
     return nums
-    // return Array.from(nums)
 }
 
 const getRandomInt = (max) => {
